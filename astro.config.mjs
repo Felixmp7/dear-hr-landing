d4df18678eeb8075b0b729ddd724a1e23b2fedb6
Felix Pacheco <devfex.19@gmail.com>
2025-02-07 18:55:48 -0400

    Update Astro configuration for server output and add Node adapter; upgrade dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+import node from '@astrojs/node';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
@@ -7,9 +8,13 @@ import robotsTxt from 'astro-robots-txt';
 // https://astro.build/config
 export default defineConfig({
     site: 'http://localhost:4321',
+    output: 'server',
     integrations: [tailwind(), sitemap(), robotsTxt()],
     i18n: {
         locales: ['en', 'es'],
         defaultLocale: 'es',
     },
+    adapter: node({
+        mode: 'standalone',
+    }),
 });
