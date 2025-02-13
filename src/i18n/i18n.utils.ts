60ce7647a0bf1eb87e54670f1741b922703262c5
Felix Pacheco <devfex.19@gmail.com>
2025-02-13 16:03:42 -0400

    Refactor translation logic by creating a custom hook and updating imports across components

--- src/i18n/i18n.utils.ts
+++ src/i18n/i18n.utils.ts
@@ -1,20 +1,12 @@
 import metaLdJson from '@/data/metaLd.json';
 import { ui } from './strings.translations';
 
-const DEFAULT_LANG = 'es';
+export const DEFAULT_LANG = 'es';
 
-export function getLangFromUrl(url: URL) {
+export const getLangFromUrl = (url: URL) => {
     const [, lang] = url.pathname.split('/');
     if (lang in ui) return lang as keyof typeof ui;
     return DEFAULT_LANG;
-}
+};
 
-export function useTranslations(lang: keyof typeof ui) {
-    return function t(key: keyof (typeof ui)[typeof DEFAULT_LANG]) {
-        return ui[lang][key] || ui[DEFAULT_LANG][key];
-    };
-}
-
-export function getMetaLdJson(lang: keyof typeof ui) {
-    return JSON.stringify(metaLdJson[lang]);
-}
+export const getMetaLdJson = (lang: keyof typeof ui) => JSON.stringify(metaLdJson[lang]);
